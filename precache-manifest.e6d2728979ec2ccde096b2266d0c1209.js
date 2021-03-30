047bc6833322aa7311d7d111f84145981d48e974
summerWu <583314838@qq.com>
2021-03-30 17:32:47 +0800

    deploy

---
+++ precache-manifest.e6d2728979ec2ccde096b2266d0c1209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd3e1f7254056651bc5",
+    "url": "/sumsumbank-website/css/app.a4a7a2a0.css"
+  },
+  {
+    "revision": "40a14e8b574ee371a7f29bb9559f6aa4",
+    "url": "/sumsumbank-website/index.html"
+  },
+  {
+    "revision": "7dd3e1f7254056651bc5",
+    "url": "/sumsumbank-website/js/app.0a4398c7.js"
+  },
+  {
+    "revision": "d11e22c2d50a3d9a7047",
+    "url": "/sumsumbank-website/js/chunk-vendors.83a8bc46.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/sumsumbank-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sumsumbank-website/robots.txt"
+  }
+]);
